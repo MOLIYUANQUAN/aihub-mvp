2d144e65663297a43bffb39644fd3fc9a69c707b
t64979zj <jiajimmy061@gmail.com>
2025-03-05 13:40:21 +0000

    second commit this should be fine

---
+++ app/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+//import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider as PaperProvider } from 'react-native-paper';
+import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatScreen';
+// import TutorialScreen from '../screens/TutorialScreen';
+// import ExploreScreen from '../screens/ExploreScreen';
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      {/* <NavigationContainer> */}
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Chat" component={ChatScreen} />
+          {/* <Tab.Screen name="Tutorial" component={TutorialScreen} />
+          <Tab.Screen name="Explore" component={ExploreScreen} /> */}
+        </Tab.Navigator>
+      {/* </NavigationContainer> */}
+    </PaperProvider>
+  );
+}
